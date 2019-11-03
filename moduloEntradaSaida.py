b59570ce7dead34596a5a5b139aae89c7477c0a4
luanfreitas5 <luan.mgf@gmail.com>
2019-11-03 17:24:18 -0300

    update

---
+++ moduloEntradaSaida.py
@@ -0,0 +1,93 @@
+"""
+    Módulo responsável pelos métodos de leitura e exibição de dados.
+"""
+
+import moduloProcessos as proc
+
+
+class LeitorArquvo:
+    """
+        Classe dos métodos de leitura dos dados
+    """
+
+    def __init__(self):
+        pass
+        
+    def leitura_arquivo_processos(self, arquivo_processo):
+        """
+            Cria a lista de processos a partir do arquivo processo
+            e retorna a lista criada
+        """
+        pid = 0
+        lista_processos = []
+        with open(arquivo_processo, "r") as arquivo_processo:
+            for linha in arquivo_processo:
+                lista_processos.append(proc.Processo([int(dado) for dado in linha.split(',')]))
+        # arruma o vetor de moduloProcessos de menor tempo a maior tempo de inicializacao
+        lista_processos.sort(key=lambda dado:dado.tempo_inicializacao, reverse=False)
+        # arruma o id de cada moduloProcessos com base no tempo de inicializacao.
+        for processo in lista_processos:
+            processo.pid = pid
+            pid += 1
+        return lista_processos
+    
+    def leitura_arquivo_file(self, arquivo_file):
+        """
+            Cria a lista de arquivos e de instruções a partir do arquivo file
+            e retorna as duas listas criadas
+        """
+        lista_arquivos = []
+        with open (arquivo_file, "r") as arquivo_file:
+            arquivo = arquivo_file.read().splitlines()
+            
+        lista_arquivos.append(int(arquivo[0]))
+        i = int(arquivo[1])
+        
+        for linha in range(2, i + 2):
+            lista_arquivos.append(arquivo[linha])
+        lista_instrucoes = []
+        for linha in range(i + 2, len(arquivo)):
+            lista_instrucoes.append(arquivo[linha])
+        return [lista_arquivos, lista_instrucoes]
+
+
+class Impressao:
+    """
+        Classe dos métodos de impressão de dados
+    """
+
+    def __init__(self):
+        """
+            Construtor da classe de Impressao
+        """
+        self.log_operacao = []
+
+    def imprimir_mapa_disco(self, mapa_disco):
+        """
+            Método para imprime o mapa do disco
+        
+        """
+        print("\n")
+        print(mapa_disco)
+
+    def imprimir_dispatcher(self, processo):
+        """
+            Método para imprime a mensagem do dispatcher do processo referenciado
+        """
+        print('\ndispatcher =>')
+        print('\tPID:\t\t {}'.format(processo.pid))
+        print('\toffset:\t\t {}'.format(processo.posicao_bloco_disco))
+        print('\tblocks:\t\t {}'.format(processo.bloco_memoria))
+        print('\tpriority:\t {}'.format(processo.prioridade))
+        print('\ttime:\t\t {}'.format(processo.tempo_processador))
+        print('\tprinters:\t {}'.format(bool(processo.codigo_impressora)))
+        print('\tscanners:\t {}'.format(bool(processo.scanner)))
+        print('\tmodems:\t\t {}'.format(bool(processo.modem)))
+        print('\tdrives:\t\t {}'.format(bool(processo.codigo_disco)))
+
+    def imprimir_log(self):
+        """
+            Método para imprime o log das operacoes executadas
+        """
+        for log in self.log_operacao:
+            print(log)
---
+++ moduloFilas.py
@@ -0,0 +1,87 @@
+"""
+    Módulo responsável pelo gerenciamento da filas.
+    Mantém as interfaces e funções que operam sobre os processos;
+"""
+
+
+class Fila():
+
+    def __init__(self):
+
+        self.lista_processo_0 = []
+        self.lista_processo_1 = []
+        self.lista_processo_2 = []
+        self.lista_processo_3 = []
+        self.lista_processo_usuario_execucao = []
+        self.lista_processo_sistema_execucao = []
+        self.lista_processo_pronto = []
+    
+    def inicializar_fila(self):
+        self.lista_processo_0 = []
+        self.lista_processo_1 = []
+        self.lista_processo_2 = []
+        self.lista_processo_3 = []
+        self.lista_processo_usuario_execucao = []
+        self.lista_processo_sistema_execucao = []
+             
+    def ordenar_filas_prioridade(self, tempo_execucao):
+        """
+            Método responsável por ordenar os processos processos 
+            na fila de prioridade, em ordem crescente de menor para maior.     
+        """
+        for processo in self.lista_processo_pronto:          
+              
+            if (processo.tempo_inicializacao <= tempo_execucao):
+                
+                if(processo.prioridade == 0):
+                    self.lista_processo_0.append(processo)
+                    
+                if(processo.prioridade == 1):
+                    self.lista_processo_1.append(processo)
+                     
+                if(processo.prioridade == 2):
+                    self.lista_processo_2.append(processo)
+                    
+                if(processo.prioridade == 3):
+                    self.fila.lista_processo_3.append(processo)
+
+    def alterar_fila_prioridade_usuario(self, tempo_execucao):
+        """
+            EVITAR STARVATION
+            Método responsável por alterar a prioridade dos processos  
+            que estiverem mais de 10 unidades de tempo esperando 
+            sem nunca terem sidos executados na fila de prioridade.
+        """       
+        for processo in self.lista_processo_2:
+          
+            if((processo.execucao == 0) and (processo.tempo_inicializacao + 9 < tempo_execucao) and (not processo.lista_prioridade)):
+                processo.lista_prioridade = True
+                self.lista_processo_1.append(processo)
+            elif((processo.execucao == 0) and (processo.tempo_inicializacao + 19 < tempo_execucao) and (processo.lista_prioridade)):
+                self.lista_processo_1.append(processo)
+       
+        for processo in self.lista_processo_3:
+            if((processo.execucao == 0) and (processo.tempo_inicializacao + 9 < tempo_execucao) and (not processo.lista_prioridade)):
+                processo.lista_prioridade = True 
+                self.lista_processo_2.append(processo)
+
+    def adicionar_processo_fila_prioridade_usuario(self):
+        """
+            Método responsável por adicionar um processo que não terminou sua execução 
+            no final na fila processo após a execucao.
+        """
+        for processo in self.lista_processo_usuario_execucao:
+            if processo.execucao >= processo.tempo_processador:
+                self.lista_entrada_saida.append(processo)
+            else:
+                if(processo.prioridade == 1):
+                    self.lista_processo_1.append(processo)
+                    self.lista_processo_usuario_pronto.append(processo)
+                  
+                if(processo.prioridade == 2):
+                    self.lista_processo_2.append(processo)
+                    self.lista_processo_usuario_pronto.append(processo)
+                   
+                if(processo.prioridade == 3):
+                    self.lista_processo_3.append(processo)
+                    self.lista_processo_usuario_pronto.append(processo)    
